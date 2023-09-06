21ab84fdf82b610b40380c1e60ffb00d447a2af2
Henrik L <helagro@outlook.com>
2023-09-06 18:15:55 +0200

    Modulerize Mongoose setup code

--- server/app.js
+++ server/app.js
@@ -1,23 +1,14 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var morgan = require('morgan');
 var path = require('path');
 var cors = require('cors');
 var history = require('connect-history-api-fallback');
 
+require("./DBhandler")
+
 // Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/animalDevelopmentDB';
 var port = process.env.PORT || 3000;
 
-// Connect to MongoDB
-mongoose.connect(mongoURI).catch(function(err) {
-    if (err) {
-        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-        console.error(err.stack);
-        process.exit(1);
-    }
-    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
-});
 
 // Create Express app
 var app = express();
