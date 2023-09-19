2b54b6cba9c2baf744b8eeae689da41110d47cac
Henrik L <helagro@outlook.com>
2023-09-19 19:48:53 +0200

    Add Courses.vue

---
+++ client/src/components/Courses.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Hi bro</p>
+</template>
+
+<script setup>
+
+// import { Api } from '@/Api'
+
+// Api.get('/posts/6509d455374dd370988806dc/courses').then((response) => {
+//   console.log(response.data)
+// })
+
+console.log('Hello')
+
+</script>
