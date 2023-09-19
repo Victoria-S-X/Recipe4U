684f886abc5c797afa66f106ffaeaffea17271fc
Henrik L <helagro@outlook.com>
2023-09-19 20:41:11 +0200

    Add courses to list

--- client/src/components/Courses.vue
+++ client/src/components/Courses.vue
@@ -1,26 +1,32 @@
 <template>
-  <p>Hi bro</p>
+  <ul>
+    <li v-for="course in courses" :key="course._id">
+      {{ course._id }}
+    </li>
+  </ul>
 </template>
 
 <script>
+
 import { Api } from '@/Api'
 
 export default {
   mounted() {
-    console.log('mounted')
 
     Api.get('/posts/6509d455374dd370988806dc/courses', {
       headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NTA5ZDQ1MzM3NGRkMzcwOTg4ODA2YzMiLCJpYXQiOjE2OTUxNDI5OTZ9.dOJ6CMBgcYb7Q0eRGtbpkxo8DyuE-8SLxUsYHQe0MRE'
       }
     }).then((response) => {
-      console.log(response.data)
-    }).catch((error) => {
-      console.log(error, 'whoops')
+      this.courses = response.data
     })
+    
+  },
+  data() {
+    return {
+      courses: []
+    }
   }
 }
-
-console.log('Hello')
 
 </script>
