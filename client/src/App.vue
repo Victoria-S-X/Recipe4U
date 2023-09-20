c2a23e78b688815cb31062bffb5773ad845d450b
shiyaox <shiyaox@student.chalmers.se>
2023-09-20 17:31:07 +0200

    Created navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <navbar></navbar>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<script>
+import Navbar from './components/Navbar.vue'
+export default ({
+  components: {
+    Navbar
+  }
+})
+</script>
