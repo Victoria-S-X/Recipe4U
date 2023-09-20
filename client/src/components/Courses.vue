26144b96ada198acbef1e4511cd3487218bf57a1
Henrik L <helagro@outlook.com>
2023-09-20 07:19:02 +0200

    Add unattend option

--- client/src/components/Courses.vue
+++ client/src/components/Courses.vue
@@ -2,7 +2,8 @@
   <div>
     <div v-for="course in courses" :key="course._id">
       {{ course._id }}
-      <button @click="attend(course._id)" >Attend</button>
+      <button v-if="isAttending()" @click="attend(course._id)" >Register</button>
+      <button v-else @click="attend(course._id)" >Unregister</button>
     </div>
   </div>
 </template>
@@ -10,6 +11,7 @@
 <script>
 
 import { Api } from '@/Api'
+import isAttending from '@/mixins/user'
 
 export default {
   mounted() {
@@ -38,8 +40,20 @@ export default {
       }).catch((error) => {
         console.log(error, error.response)
       })
+    },
+    unAttend(courseID) {
+      Api.delete(`/attendance/${courseID}`, {
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NTA5ZDQ1MzM3NGRkMzcwOTg4ODA2YzMiLCJpYXQiOjE2OTUxNDI5OTZ9.dOJ6CMBgcYb7Q0eRGtbpkxo8DyuE-8SLxUsYHQe0MRE'
+        }
+      }).then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        console.log(error, error.response)
+      })
     }
-  }
+  },
+  mixins: [isAttending]
 }
 
 </script>
